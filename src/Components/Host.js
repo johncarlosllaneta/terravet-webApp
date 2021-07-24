ee49c06acee40f889a7c7ab490a2fee59e4cf8f5
Jhaycee <43661903+johncarlosllaneta@users.noreply.github.com>
2021-07-24 22:23:41 +0800

    update web app

--- src/Components/Host.js
+++
@@ -1,4 +0,0 @@
-const hostUrl = 'http://localhost:3001';
-
-const newsAPI = '3e311781a1794a6ca438e0921658af66';
-export { hostUrl, newsAPI };
--- src/Pages/SystemAdmin/ModalScheduleViewer.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react'
-import { Modal, Button, Row, Col, Form } from "react-bootstrap";
-
-function ModalScheduleViewer(props) {
-
-
-}
-
-export default ModalScheduleViewer
